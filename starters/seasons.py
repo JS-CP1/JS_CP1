406d958c9fabfe744b4fbd76b33b66cefd5b4df5
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-08-21 09:05:11 -0600

    Seasons Starter

---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+#JS, 1st, Seasons Starter
+
+last_name = input("What is your last name?\n").capitalize().strip()
+fav_season = input("What is your favorite season?\n").lower().strip()
+print("Wow, ", last_name, "san, I like ", fav_season, " too!")
