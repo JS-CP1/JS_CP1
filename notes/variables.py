c73a8b6eec2fbab694382c5168569022e7d80a0b
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-08-21 09:52:08 -0600

    Variable Notes

---
+++ notes/variables.py
@@ -0,0 +1,7 @@
+#JS, 1st, Variable Notes
+even_number = 2
+grade = 99
+animal = "Parrot"
+print(grade)
+grade = 95
+print(grade)
--- starters/seasons.py
+++ starters/seasons.py
@@ -1,5 +1,5 @@
-#JS, 1st, Seasons Starter
-
+#JS, 1st, Seasons
 last_name = input("What is your last name?\n").capitalize().strip()
-fav_season = input("What is your favorite season?\n").lower().strip()
-print("Wow, "+last_name +"-san, I like "+ fav_season +" too!")
+season = input("What is your favorite season?\n").lower().strip()
+print("Wow, "+ last_name +"-san, I like "+ season +" too!")
+#You could also do: print(f"Wow, {last_name}-san, I like {season} too!")
