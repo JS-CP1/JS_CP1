181a98ad2d3b187170652523613ed2f9a1c124e0
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-08-28 08:58:41 -0600

    Number Notes

---
+++ notes/numbers.py
@@ -0,0 +1,36 @@
+# JS, 1st, Integers and Floats Notes
+
+# Integers
+age = 99999999999999999
+year = 2015
+room = 200
+hour = 9
+motivation = -5
+
+# Floats (Floatin point number)
+pi = 3.141592653589793238462643383279502884197163993751058209749445923078164062862089986280348253421170
+percent = -.75
+damage = 9.75
+
+# Math
+print(5+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2) #2.5
+print(5//2) #2
+print(5%2) #1
+print(5**2) #25
+
+print(5+2*9+(5**5)%3)
+
+# Type Casting
+num = int(3.14)
+print(num)
+
+# Using Variables
+hours = 67
+minutes = 54
+print(hours*60+minutes)
+
+age = int(input("What is your age?\n"))
+print("Dang you old,", age*365*24*60, "minutes old")
