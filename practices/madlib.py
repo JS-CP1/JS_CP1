5592d44cbcb1299d9154b63d331f655e89d9c49a
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-08-29 09:52:41 -0600

    Madlib Update 1

---
+++ practices/madlib.py
@@ -0,0 +1,10 @@
+# JS, 1st, Madlib Assignment
+
+verb1 = input("Choose a verb:\n")
+verb2 = input("Choose a verb:\n")
+adj1 = input("Choose a verb:\n")
+adj2 = input("Choose a verb:\n")
+noun1 = input("Choose a verb:\n")
+noun2 = input("Choose a verb:\n")
+
+print(f"The duck {verb1} up to the lemonade stand and he {verb2} to the man, running the = stand")
