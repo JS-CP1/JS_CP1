f4784123505f8f3fa2465eeb87ce30f4be3852e8
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-09-03 09:53:18 -0600

    Madlib Finished

--- practices/madlib.py
+++ practices/madlib.py
@@ -1,97 +1,95 @@
 # JS, 1st, Madlib Assignment
 
-verb1 = input("Choose a verb:\n")
-verb2 = input("Choose a verb:\n")
-adj1 = input("Choose an adjective:\n")
-adj2 = input("Choose an adjective:\n")
-waddle = input("Choose an adjective:\n")
+waddle = input("Choose an verb:\n")
+stand = input("Choose a noun:\n")
 lemonade = input("Choose a noun:\n")
 grapes = input("Choose a noun:\n")
+duck = input("Choose a noun:\n")
 
 print(f"""
-Tell me the duck story
+Tell me the """ + duck + """ story
 Bum bum bum, ba-dum ba-dum
 
-A duck walked up to a {lemonade} stand
-And he said to the man, running the stand
-"Hey! (Bum bum bum) Got any {grapes}?"
+A """ + duck + """ walked up to a """ + lemonade + " " + stand + """
+And he said to the man, running the """ + stand + """
+"Hey! (Bum bum bum) Got any """ + grapes + """?"
 
-The man said "No, we just sell {lemonade}
+The man said "No, we just sell """ + lemonade + """
 But it’s cold and it's fresh, and it’s all home-made.
 Can I get you a glass?"
-The duck said “I’ll pass.”
+The """ + duck + """ said “I’ll pass.”
 
-Then he {waddle}d away, {waddle} {waddle}
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
 'Til the very next day, bum bum bum bum bum ba-dum
 
-When the duck walked up to the {lemonade} stand
-And he said to the man, running the stand
-"Hey! (Bum bum bum) Got any {grapes}?
+When the """ + duck + """ walked up to the """ + lemonade + " " + stand + """
+And he said to the man, running the """ + stand + """
+"Hey! (Bum bum bum) Got any """ + grapes + """?
 
 The man said "No, like I said yesterday
-We just sell {lemonade}. Okay?
+We just sell """ + lemonade + """. Okay?
 Why not give it a try?"
-The duck said "Goodbye."
+The """ + duck + """ said "Goodbye."
 
-Then he {waddle}d away, {waddle} {waddle}
-Then he {waddle}d away, {waddle} {waddle}
-Then he {waddle}d away, {waddle} {waddle}
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
 'Til the very next day, bum bum bum bum bum ba-dum
 
-When the duck walked up to the {lemonade} stand
-And he said to the man running the stand
-"Hey! (Bum bum bum) Got any {grapes}?
+When the """ + duck + """ walked up to the """ + lemonade + " " + stand + """
+And he said to the man running the """ + stand + """
+"Hey! (Bum bum bum) Got any """ + grapes + """?
 
 The man said "Look, this is getting old
-I mean, {lemonade}’s all we’ve ever sold
+I mean, """ + lemonade + """’s all we’ve ever sold
 Why not give it a go?"
-The duck said “How 'bout, no.”
+The """ + duck + """ said “How 'bout, no.”
 
-Then he {waddle}d away, {waddle} {waddle}
-Then he {waddle}d away, {waddle} {waddle} {waddle}
-Then he {waddle}d away, {waddle} {waddle}
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + " " + waddle + """
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
 'Til the very next day, bum bum bum bum bum ba-dum
 
 [Verse 4]
-When the duck walked up to the {lemonade} stand
-And he said to the man running the stand
-"Hey! (Bum bum bum) Got any {grapes}?"
+When the """ + duck + """ walked up to the """ + lemonade + " " + stand + """
+And he said to the man running the """ + stand + """
+"Hey! (Bum bum bum) Got any """ + grapes + """?"
 
 
-The man said "THAT’S IT! If you don’t stay away, Duck
+The man said "THAT’S IT! If you don’t stay away, """ + duck + """
 I’ll glue you to a tree and leave you there all day, stuck
 So don’t get too close!"
-The duck said "Adios."
+The """ + duck + """ said "Adios."
 
-Then he {waddle}d away, {waddle} {waddle}
-Then he {waddle}d away, {waddle} {waddle} {waddle}
-Then he {waddle}d away, {waddle} {waddle}
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + " " + waddle + """
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
 'Til the very next day, bum bum bum bum bum ba-dum
 
-When the duck walked up to the {lemonade} stand
-And he said to the man running the stand
+When the """ + duck + """ walked up to the """ + lemonade + " " + stand + """
+And he said to the man running the """ + stand + """
 "Hey! (Bum bum bum) got any glue?"
 "What?" "Got any glue?"
 "No, why would I– oh!"
 Then one more question for you
-"Got any {grapes}?"
+"Got any """ + grapes + """?"
 (Bum bum bum, bum bum bum)
 
 And the man just stopped, then he started to smile
 He started to laugh, he laughed for a while
-He said “Come on duck, let’s walk to the store
-I’ll buy you some {grapes} so you won’t have to ask anymore.”
+He said “Come on """ + duck + """, let’s walk to the store
+I’ll buy you some """ + grapes + """ so you won’t have to ask anymore.”
 
-So they walked to the store and the man bought some {grapes}
-He gave one to the duck and the duck said “Hmm... No thanks
+So they walked to the store and the man bought some """ + grapes + """
+He gave one to the """ + duck + """ and the """ + duck + """ said “Hmm... No thanks
 
 But you know what sounds good?
 It would make my day
 Do you think this store...
 Do you think this store...
 Do you think this store...
-Has any… {lemonade}?”
+Has any… """ + lemonade + """?”
 
-Then he {waddle}d away, {waddle} {waddle}
-Then he {waddle}d away, {waddle} {waddle} {waddle}
-Then he {waddle}d away, {waddle} {waddle}""")
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + " " + waddle + """
+Then he """ + waddle + """ed away, """ + waddle + " " + waddle + """""")
