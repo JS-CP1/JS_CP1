63fc39864d5228792cdaf63d50c961d9cd2254e4
futashikana <joseph.stratford@ucas-edu.net>
2025-09-05 22:47:55 +0000

    baka desu

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -1,28 +1,24 @@
 #JS, 1st, Idiot Proof Practice
 name = input("What is your name?\n")
-try:
-    while "1" in name or "2" in name or "3" in name or "4" in name or "5" in name or "6" in name or "7" in name or "8" in name or "9" in name or "0" in name:
-        name = input("That is not a valid name. What is your name?\n")
-except ValueError:
-    pass
+invalid_characters = "0123456789!@#$%^&*()-=_+[]{}|:;>.<,~`"
+while any(char in invalid_characters for char in name):
+    name = input("That is not a valid name. What is your name?\n")
 while True:
     gpa_str = input("What is your gpa?\n")
-    try:
+    if gpa_str.replace('.', '', 1).isdigit() or (gpa_str.startswith('-') and gpa_str[1:].replace('.', '', 1).isdigit()):
         gpa = float(gpa_str)
         if 0.0 <= gpa <= 4.0:
             break
         else:
             print("Gpa must be between 0.0 and 4.0.")
-    except ValueError:
+    else:
         print("That was not a valid gpa. Please try again.")
-phone1 = []
-gpa = round(float(gpa), 1)
 phone = input("What is your phone number?\n").strip(" ").strip("-")
-while len(phone) != 10:
+invalid_characters2 = "qwertyuiopasdfghjkllzxcvbnm,.;'!@#$%^&*()~`_-+={[]}|:><"
+while len(phone) != 10 or any(char in invalid_characters2 for char in name):
     phone = input("That's not a valid phone number. What is your phone number?\n").strip(" ").strip("-")
-phone1 = phone.split(:phone1[2])
-print(phone1)
-phone2 = phone1[0].split(phone1[0][0])
-
-phone_number = phone1 + "-" + phone2 + "-"
-print(phone_number)
+char_phone = list(phone)
+char_phone.insert(3, ' ')
+char_phone.insert(7, ' ')
+phone_number = "".join(char_phone)
+print(f"Hi {name}, you have a gpa of {gpa} and your phone number is {phone_number}")
