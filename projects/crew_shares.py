2709e3cdbf14c679f6810cee904d03ebae84f3f1
futashikana <joseph.stratford@ucas-edu.net>
2025-09-11 16:11:44 +0000

    Crew Shares Finished

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -1,4 +1,10 @@
 # JS, 1st, Crew Shares 
-
-total_money = int(input("How much money was there total?").strip().strip("$"))
-crew_members = int(input("How many crew members?"))
+print("\033c", end="")
+total_money = int(input("How much money (in total) did you get from the excursion?\n ").strip().strip("$"))
+crew_members = int(input("How many crew members? (Does not include Captain and First Mate)\n "))
+share = total_money/(crew_members+10)
+crew_remainder = share - 500
+first_mate = share * 3
+captain = share * 7
+print("\033c", end="")
+print(f"The captain gets ${captain:.2f}.\nThe first mate gets ${first_mate:.2f}.\nThe crew still needs ${crew_remainder:.2f} to each of them.")
