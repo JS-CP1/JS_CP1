24a00850deaa07ab5a2152b3a91c2f9dc7c9cbc3
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-09-15 09:47:54 -0600

    Crew Shares OPTIMIZATION

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -1,10 +1,5 @@
-# JS, 1st, Crew Shares 
-print("\033c", end="")
-total_money = int(input("How much money (in total) did you get from the excursion?\n ").strip().strip("$"))
-crew_members = int(input("How many crew members? (Does not include Captain and First Mate)\n "))
+# JS, 1st, Crew Shares
+total_money, crew_members = [int(input("How much money (in total) did you get from the excursion?\n ").strip().strip("$")), int(input("How many crew members? (Does not include Captain and First Mate)\n ").strip())]
 share = total_money/(crew_members+10)
-crew_remainder = share - 500
-first_mate = share * 3
-captain = share * 7
-print("\033c", end="")
+crew_remainder, first_mate, captain = [share - 500, share * 3, share * 7]
 print(f"The captain gets ${captain:.2f}.\nThe first mate gets ${first_mate:.2f}.\nThe crew still needs ${crew_remainder:.2f} to each of them.")
