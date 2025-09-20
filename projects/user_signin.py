ff99c28f96ecc4e2a7bc986ec81ab8241915cae2
futashikana <joseph.stratford@ucas-edu.net>
2025-09-20 05:39:49 +0000

    User Sign In Project Finished

---
+++ projects/user_signin.py
@@ -0,0 +1,8 @@
+# JS, 1st, User Sign In Project
+info = [[input("Insert a username:\n").strip().lower(), input("Insert a password:\n").strip().lower()] for _ in range(int(input("How many accounts would you like to add?\n").strip()))]
+username, password = [input("What is your username?\n").strip().lower(), input("What is your password?\n").strip().lower()]
+for inf in info:
+    if inf[0] == username and inf[1] == password:
+        print("You succesfully logged in.")
+    else:
+        print("That was not a valid username and or password.")
