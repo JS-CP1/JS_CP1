eddad41d88d17ee48ab71ac60f31ce52da7c595f
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-09-22 09:53:14 -0600

    User Sign In maybe complete this time

--- practices/user_signin.py
+++ practices/user_signin.py
@@ -1,5 +1,7 @@
 # JS, 1st, User Sign In Practice
-info = [[input("Insert a username:\n").strip().lower(), input("Insert a password:\n").strip().lower()] for _ in range(int(input("How many accounts would you like to add?\n").strip()))]
+info = []
+for _ in range(int(input("How many accounts would you like to add?\n").strip())):
+    info.append([input("Insert a username:\n").strip().lower(), input("Insert a password:\n").strip().lower()])
 username, password = [input("What is your username?\n").strip().lower(), input("What is your password?\n").strip().lower()]
 for inf in info:
     if inf[0] == username and inf[1] == password:
