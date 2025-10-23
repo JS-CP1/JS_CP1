2cfbb20143f83131815a03d0b721b4f86b4e5917
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-10-23 09:26:44 -0600

    29 gofling is my favvv

--- golf/caesar_cypher_golf.py
+++ golf/caesar_cypher_golf.py
@@ -1,3 +1,3 @@
 #o=input();s=int(input());s=-s if o=='d'else s;m=input();r=[chr((ord(c)-97+s)%26+97)if 'a'<=c<='z'else chr((ord(c)-65+s)%26+65)if 'A'<=c<='Z'else c for c in m];print(''.join(r)) 176 characters
 
-#s=int(input());s=-s if input()=="d"else s; r=[chr((ord(c)-97+s)%26+97)if 'a'<=c<='z'else chr((ord(c)-65+s)%26+65)if 'A'<=c<='Z'else c for c in input()];print(''.join(r)) 169 characters
+#s=int(input());s=-s if input()=="d"else s;r=[chr((ord(c)-97+s)%26+97)if 'a'<=c<='z'else chr((ord(c)-65+s)%26+65)if 'A'<=c<='Z'else c for c in input()];print(''.join(r)) 168 characters
