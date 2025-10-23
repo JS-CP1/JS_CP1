a854ff5031ea7b45c7192b02380ff27fbfae0683
futashikana <joseph.stratford@ucas-edu.net>
2025-10-23 02:34:09 +0000

    27 GOOOOOOOOOLLLLLLLLLFFFFFFFFFF

---
+++ golf/average_grade_golf.py
@@ -0,0 +1,8 @@
+#-------------------------- one digit numbers -----------------------------------
+#a=input();b=[input()for _ in a];c=sum(map(int,a))/len(a);print(c) 62 characters
+
+#a=input();c=sum(map(int,a))/len(a);print(c) 43 characters
+
+#print(sum(map(int,a:=input()))/len(a)) 38 characters
+#-------------------------- any digit numbers -----------------------------------
+#l=[int(input())for _ in range(int(input()))];print(sum(l)/len(l)) 65 characters
---
+++ golf/basic_calculator_golf.py
@@ -0,0 +1,7 @@
+#e=input();a=int(input());b=int(input());print(a+b)if e=="+"else print(a-b)if e=="-"else print(a*b)if e=="*" else print(a/b) 123 characters
+
+#e,a,b=input(),int(input()),int(input());print(eval(f"{a}{e}{b}")) 65 characters
+
+#print(eval(f"{int(input())}{input()}{int(input())}")) 53 characters
+ 
+#exec("print("input()")") 24 characters but kinda scammy
