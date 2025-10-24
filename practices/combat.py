0c4bfb1ecc13165d557064713cc7cbc641d714d8
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-10-24 09:50:23 -0600

    32 Combat Program Actually Finished This Time I Swear Please Don't Sue Me

--- practices/combat.py
+++ practices/combat.py
@@ -1,33 +1,41 @@
 # JS, 1st, Combat Program
 import random
+def user_turn():
+    action = input("What would you like to do?\n ")
+    while action != "a" and action != "d":
+        action = input("That was not a valid input. What would you like to do? (a, d)\n ")
+    return action
+def boss_turn():
+    action = "a"
+    return action
 user_stats = {
     "melee":{
         "hp": 200,
         "def": 10,
-        "at": 15,
+        "at": 35,
         "hit": .8,
-        "hurt": .99
+        "hurt": .8
     },
     "ranger":{
         "hp": 100,
         "def": 2,
-        "at": 10,
+        "at": 15,
         "hit": .5,
         "hurt": .1
     },
     "mage":{
         "hp": 150,
         "def": 5,
-        "at": 12,
+        "at": 25,
         "hit": .8,
         "hurt": .5
     },
     "summoner":{
-        "hp": 50,
+        "hp": 20,
         "def": 0,
         "at": 20,
         "hit": 1,
-        "hurt": .1
+        "hurt": .05
     }
 }
 user = {
@@ -42,11 +50,11 @@ boss_stats = {
         "hp": 250,
         "at": 15
     },
-    "dog":{
+    "dog": {
         "hp": 500,
         "at": 30
     },
-    "yharon":{
+    "yharon": {
         "hp": 1000,
         "at": 50
     },
@@ -65,25 +73,21 @@ while type != "melee" and type != "ranger" and type != "mage" and type != "summo
     type = input("That was not a valid class. What class would you like to play? (Melee, Ranger, Mage, Summoner)\n ")
 for i in user:
     user[i] = user_stats[type][i]
-boss = input("What boss would you like to fight? (Providence, DOG, Yharon, Calamitas)\n ").lower().strip()
-while boss != "providence" and boss != "dog" and boss != "yharon" and boss != "calamitas":
-    boss = input("That was not a valid boss. What boss would you like to fight? (Providence, DOG, Yharon, Calamitas)\n ")
-for i in boss:
-    boss[i] = boss_stats[boss][i]
-print(f"You have chosen the {type} class, and are fighting {boss}.\nWhen you are given the option, press (a) to attack and (d) to defend")
+boss_choice = input("What boss would you like to fight? (Providence, DOG, Yharon, Calamitas)\n ").lower().strip()
+while boss_choice != "providence" and boss_choice != "dog" and boss_choice != "yharon" and boss_choice != "calamitas":
+    boss_choice = input("That was not a valid boss. What boss would you like to fight? (Providence, DOG, Yharon, Calamitas)\n ")
+for x in boss:
+    boss[x] = boss_stats[boss_choice][x]
+print(f"You have chosen the {type} class, and are fighting {boss}.\nWhen you are given the option, press (a) to attack and (d) to dodge")
 if random.randint(0,1) == 1:
     turn = "user"
 else:
     turn = "boss"
 while combat == True:
     if turn == "user":
-        action = input("What would you like to do?")
-        while action != "a" and action != "d":
-            action = input("That was not a valid input. What would you like to do? (a, d)")
-        turn = "boss"
+        action = user_turn()
     else:
-        action = "a"
-        turn = "user"
+        action = boss_turn()
     if action == "a":
         if turn == "user":
             if random.random() < user["hit"]:
@@ -96,14 +100,20 @@ while combat == True:
                 user["hp"] = (user["hp"] + user["def"]) - boss["at"]
                 print(f"You took {boss["at"] - user["def"]} damage.")
             else:
-                print("You dodged!")
+                print("The enemy attacks, but you dodged!")
     else:
         print("Nothing happens...")
         continue
     print(f"Your hp: {user["hp"]}\nBoss hp: {boss["hp"]}")
+    if turn == "user":
+        turn = "boss"
+    else:
+        turn = "user"
     if user["hp"] > 0 and boss["hp"] > 0:
         continue
     if user["hp"] > 0:
         print("You Won!")
+        exit()
     else:
         print("You Lost...")
+        exit()
