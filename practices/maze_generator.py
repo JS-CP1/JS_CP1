afdf7083f51d34af43694e8b00ed1c063ab39fb9
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-10-31 09:53:06 -0600

    39 Maze Started

---
+++ practices/maze_generator.py
@@ -0,0 +1,31 @@
+# JS, 1st, Maze Generator
+from turtle import *
+import random
+maze = {
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+    11: [],
+    12: [],
+    13: [],
+    14: [],
+    15: [],
+    16: [],
+    17: [],
+    18: [],
+    19: [],
+    20: [],
+}
+for i in range(10):
+    maze[0].append("-")
+    maze[20].append("-")
+maze[0][random.randint(1,9)] = " "
+maze[20][random.random(1,9)] = " "
