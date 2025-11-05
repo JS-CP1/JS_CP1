59641b0b0d06778be68cf829ea0bbcb45ebae684
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-11-05 09:53:20 -0700

    47 fractals again

--- fun/fractals.py
+++ fun/fractals.py
@@ -3,7 +3,7 @@ from turtle import *
 screen = Screen()
 screen.setup(500, 500)
 screen.title("Fractals")
-screen.tracer()
+screen.tracer(0)
 
 draw = Turtle()
 draw.speed(0)
@@ -11,26 +11,6 @@ draw.penup()
 draw.hideturtle()
 
 drawn = [["" for _ in range(10)] for _ in range(10)]
-def draw_grid():
-    screen.tracer(0)
-    draw.pendown
-    for i in range(11):
-        draw.penup()
-        draw.goto(-250 + i * 50, 250)
-        draw.setheading(270)
-        draw.pendown()
-        draw.forward(10 * 50)
-    for i in range(11):
-        draw.penup()
-        draw.goto(-250, 250 - i * 50)
-        draw.setheading(0)
-        draw.pendown()
-        draw.forward(10 * 50)
-    draw.penup()
-    screen.update()
-    screen.tracer()
-
-draw_grid()
 
 def draw_square(row, col, fill):
     x = -250 + col * 50
@@ -38,25 +18,31 @@ def draw_square(row, col, fill):
     draw.goto(x, y)
     draw.setheading(0)
 
-    color = "black" if fill else "white"
+    if fill == True:
+        color = "black"
+    else:
+        color = "white"
     draw.fillcolor(color)
-    draw.pencolor("black")
-
-    if fill == False:
-        draw_grid()
-
+    draw.pencolor(color)
     draw.begin_fill()
+    draw.pendown()
     for _ in range(4):
         draw.forward(50)
         draw.right(90)
+    draw.penup()
     draw.end_fill()
 
 def draw_base(x, y):
     col = int((x + 250) // 50)
     row = int((250 - y) // 50)
     if 0 <= row < 10 and 0 <= col < 10:
-        drawn[row][col] = "" if drawn[row][col] == "X" else "X"
-        draw_square(row, col, fill=(drawn[row][col] == "X"))
+        if drawn[row][col] == "X":
+            drawn[row][col] = ""
+            draw_square(row, col, False)
+        else:
+            drawn[row][col] = "X"
+            draw_square(row, col, True)
+    screen.update()
 
 screen.onclick(draw_base)
 done()
