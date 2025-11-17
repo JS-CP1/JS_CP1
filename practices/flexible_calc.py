c5eb229255f783e6401b3da3415bb0bc453b19b8
futashikana <joseph.stratford@ucas-edu.net>
2025-11-17 19:45:58 +0000

    57 monke

--- practices/flexible_calc.py
+++ practices/flexible_calc.py
@@ -1,1 +1,15 @@
 # JS, 1st, Flexible Calculator
+
+def enter_number():
+    numbers = []
+    number = input("Enter numbers (type 'done' when finished):\nNumber: ")
+    while number != "done":
+        if isinstance(number, (int, float)):
+            numbers.append(number)
+        else:
+            number = input("That was not a number")
+        number = input("Number: ")
+    if len(numbers) == 0:
+        print("Your list is empty.")
+        enter_number()
+print("Welcome to the Flexible Calculator!\nAvaialable Operations: sum, average, max, min, product")
