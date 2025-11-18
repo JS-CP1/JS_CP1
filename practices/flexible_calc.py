1f6acf5f4f038ce38e30b65768baf8c838f07090
JS-CP1 <joseph.stratford@ucas-edu.net>
2025-11-18 09:53:11 -0700

    59 Flexible calc

--- practices/flexible_calc.py
+++ practices/flexible_calc.py
@@ -2,14 +2,67 @@
 
 def enter_number():
     numbers = []
-    number = input("Enter numbers (type 'done' when finished):\nNumber: ")
-    while number != "done":
-        if isinstance(number, (int, float)):
-            numbers.append(number)
-        else:
-            number = input("That was not a number")
+    print("Enter numbers (type 'done' when finished):")
+
+    while True:
         number = input("Number: ")
+
+        if number == "done":
+            break
+
+        try:
+            int(number)
+        except ValueError:
+            try:
+                float(number)
+            except ValueError:
+                print("That was not a number.")
+                continue
+        
+        numbers.append(number)
+
     if len(numbers) == 0:
         print("Your list is empty.")
-        enter_number()
-print("Welcome to the Flexible Calculator!\nAvaialable Operations: sum, average, max, min, product")
+        return enter_number()
+
+    return numbers
+
+def calculate(op, numbers):
+    result = 1
+    if op == "sum":
+        result = sum(numbers)
+    elif op == "average":
+        result = sum(numbers) / len(numbers)
+    elif op == "max":
+        result = max(numbers)
+    elif op == "min":
+        result = min(numbers)
+    elif op == "product":
+        for num in numbers:
+            result *= num
+    return result
+
+def main():
+    print("Avaialable Operations: sum, average, max, min, product")
+
+    op = input("Which operation would you like to perform? ")
+    while op != "sum" and op != "average" and op != "max" and op != "min" and op != "product":
+        op = input("That was not a valid operation. Which operation would you like to perform? ")
+
+    numbers = enter_number()
+
+    print(f"Calculating {op} of: ", end = "")
+    for num in numbers:
+        print(num, end = ", ")
+
+    result = calculate(op, numbers)
+    print(f"Result: {result}")
+
+    again = input("Would you like to perform another calculation? (yes/no) ")
+    if again == "no":
+        print("Thank you for using the Flexible Calculator!")
+    else:
+        main()
+
+print("Welcome to the Flexible Calculator!")
+main()
